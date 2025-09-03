f7990495180e77323319db7d5f10dcba7b409387
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-09-02 23:46:28 -0300

    feat: funções para inserção e consulta de dados
    Novos métodos para a classe FireStoreDB, para consulta de collections e documentos e para inserção de documentos nas collections.

--- persistence.py
+++ persistence.py
@@ -1,10 +1,51 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 
 class FireStoreDB:
     _instance = None
-    def __new__(cls, key_path=None):
+    def __new__(cls, key_path=None): # instância da classe (Singleton)
         if cls._instance is None:
             cls._instance = super(FireStoreDB, cls).__new__(cls)
             firebase_admin.initialize_app(credentials.Certificate(key_path))
+            cls._instance.db = firestore.client()
         return cls._instance
+    
+    def get_collection(self, collection, raw = False): # retorna todos os documentos da collection ou a collection em si (CollectionReference)
+        docs = self.db.collection(collection) if collection.__class__.__name__ == "str" else collection
+        if raw:
+            return docs
+        response = []
+        for doc in docs.get():
+            response.append(doc.to_dict())   
+        return response
+    
+    def insert_into_collection(self, collection, document: dict): # insere um documento em uma determinada collection
+        try:
+            target = self.db.collection(collection) if collection.__class__.__name__ == "str" else collection
+            target.add(document)
+        except:
+            return False
+        finally:
+            return True
+        
+    def get_document_by_value(self, collection, field: str, value, raw = False): # retorna documento(s) a partir de um valor de determinado campo; em dict ou em DocumentSnapshot
+        target = self.db.collection(collection) if collection.__class__.__name__ == "str" else collection
+        if raw:
+            return target.where(field, "==", value).get()
+        response = []
+        for doc in target.where(field, "==", value).get():
+            response.append(doc.to_dict())
+        return response
+    
+    def get_document_by_id(self, collection, document_id, raw = False):
+        target = (self.db.collection(collection) if collection.__class__.__name__ == "str" else collection).document(document_id)
+        if raw:
+            return target
+        return target.get().to_dict()    
+
+        
+    
+    
+
+
+        
--- routes.py
+++ routes.py
@@ -1,6 +1,26 @@
-from main import app
+from main import app, database
+from flask import jsonify, request
 
 
-@app.route("/")
+@app.route("/") # teste
 def hello_world():
     return "Hello World"
+
+@app.route("/collection", methods=["POST"]) # teste
+def get_collection():
+    return jsonify(database.get_collection(request.form.get("collection")))
+
+@app.route("/collection/add", methods=["POST"]) # teste
+def add_to_collection():
+    collection,document = request.get_json().values()
+    return "Added" if database.insert_into_collection(collection, document) else "Failed to add"
+
+@app.route("/collection/document", methods=["POST"]) # teste
+def get_document_by_value():
+    collection,field,value = request.get_json().values()
+    return jsonify(database.get_document_by_value(collection, field, value))
+
+@app.route("/collection/document/id", methods=["POST"]) # teste
+def get_document_by_id():
+    collection,document_id = request.get_json().values()
+    return jsonify(database.get_document_by_id(collection, document_id))
